ce0bb3929690332cae45d95d96c2f0e91b0e815b
Albert Calbet <albert.calbet@goldenspear.com>
2020-01-09 17:52:04 +0100

    nodejs version

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM alpine:latest
-
-RUN apk add --no-cache curl
-
-WORKDIR /root
-
-COPY entrypoint.sh .
-
-ENTRYPOINT [ "/root/entrypoint.sh" ]
---
+++ index.js
@@ -0,0 +1,83 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fetch = require('node-fetch');
+
+(async function main() {
+  try {
+    const transforms = {
+      boolean: [
+        'as_user',
+        'link_names	',
+        'mrkdwn',
+        'reply_broadcast',
+        'unfurl_links',
+        'unfurl_media',
+      ],
+      number: ['thread_ts'],
+    };
+
+    const channel = core.getInput('channel');
+    const text = core.getInput('text');
+    const as_user = core.getInput('as_user');
+    const attachments = core.getInput('attachments');
+    const blocks = core.getInput('blocks');
+    const icon_emoji = core.getInput('icon_emoji');
+    const icon_url = core.getInput('icon_url');
+    const link_names = core.getInput('link_names');
+    const mrkdwn = core.getInput('mrkdwn');
+    const parse = core.getInput('parse');
+    const reply_broadcast = core.getInput('reply_broadcast');
+    const thread_ts = core.getInput('thread_ts');
+    const unfurl_links = core.getInput('unfurl_links');
+    const unfurl_media = core.getInput('unfurl_media');
+    const username = core.getInput('username');
+    
+    let body = {
+      channel,
+      text,
+      as_user,
+      attachments,
+      blocks,
+      icon_emoji,
+      icon_url,
+      link_names,
+      mrkdwn,
+      parse,
+      reply_broadcast,
+      thread_ts,
+      unfurl_links,
+      unfurl_media,
+      username,
+    };
+    
+    for (const [k, v] of body) {
+      if (v === undefined) {
+        delete body[k];
+      } else if (transforms.boolean.includes(k)) {
+        body[k] = v === 'true' ? true : false;
+      } else if (transforms.number.includes(k)) {
+        body[k] = parseFloat(v);
+      }
+    }
+
+    const res = await fetch('https://slack.com/api/chat.postMessage', {
+      method: 'POST',
+      header: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: JSON.stringify(body),
+    });
+
+    const json = await res.json();
+    
+    if (res.ok) {
+      console.log(json);
+    } else {
+      console.error(json);
+      throw new Error(`Request failed with status ${res.status}`)
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+    process.exit(1);
+  }  
+})()
